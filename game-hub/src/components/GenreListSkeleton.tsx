d87a1c231fde99393071edac74cdc6a8bf6600b3
alexis <johnalexissaldua06@gmail.com>
2026-06-05 14:50:33 +0800

    Show skeleton while fetching the genre

---
+++ game-hub/src/components/GenreListSkeleton.tsx
@@ -0,0 +1,15 @@
+import { HStack, List, Skeleton, SkeletonText } from '@chakra-ui/react'
+import React from 'react'
+
+const GenreListSkeleton = () => {
+  return (
+    <List.Root>
+      <HStack>
+        <Skeleton></Skeleton>
+        <SkeletonText></SkeletonText>
+      </HStack>
+    </List.Root>
+  )
+}
+
+export default GenreListSkeleton
